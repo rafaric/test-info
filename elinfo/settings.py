5ea5cf1c63708c4b9884924b39f852ccd0b600c8
Rafaric <rafaelstrongoli@gmail.com>
2024-09-27 22:19:38 -0300

    settings modificado

--- elinfo/settings.py
+++ elinfo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-46m4y4&cjcim^cf%+^-(k*q$pq%%4r$68thq4f+vmc+*(qco-x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
